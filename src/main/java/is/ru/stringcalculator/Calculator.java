e71ade43010b2f202391fd79c858e9904761b003
gustavhjortur <gustav.hjortur@gmail.com>
2016-10-13 16:28:01 +0000

    first simple test passed@

---
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -0,0 +1,12 @@
+package is.ru.stringcalculator;
+
+public class Calculator {
+
+	public static int add(String text){
+		if(text.equals("")){
+			return 0;
+		}
+		else
+			return 1;
+	}
+}
---
+++ src/stringCalculator.java
@@ -0,0 +1,15 @@
+/*
+	gustav13@ru.is
+	ru.is T-303-HUGB, Hugbúnaðarfræði, 2016-3
+	TDD Exercise
+*/
+package stringCalculator;
+
+public class stringCalculator {
+    public static int add(String text){
+        if(text.equals("")){
+            return 0;
+        }
+        return 1;
+    }
+}
